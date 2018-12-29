d7a14c65d12b76e50830a8a797ee920272f6f326
cliverliu@163.com <cliverliu@163.com>
2018-12-29 09:24:55 +0800

    封装页面路径跳转方法

--- src/main/java/com/activiti/boot/web/WebEntrance.java
+++ src/main/java/com/activiti/boot/web/WebEntrance.java
@@ -1,12 +1,27 @@
 package com.activiti.boot.web;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.activiti.boot.service.SysMenuService;
 
 @Controller
-@RequestMapping("/abd")
+@RequestMapping("/abd/getView")
 public class WebEntrance {
+	
+	@Autowired
+	private SysMenuService sysMenuService;
+	
+	@GetMapping("/path/{param}")
+	private String getViewPath(@RequestParam String param) {
+		Map<String, Object> viewUrl = sysMenuService.getViewUrl(param);
+		return String.valueOf(viewUrl.get("ViewUrl"));
+	}
 
 	@GetMapping("/gologin")
 	private String goLogin() {
